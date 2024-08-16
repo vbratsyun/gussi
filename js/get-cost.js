b52fd73d8fa23c3c8503e3975529dcce1477d3c1
vbratsyun <91077541+vbratsyun@users.noreply.github.com>
2024-08-16 16:01:31 +0000

    Deploying to gh-pages from @ vbratsyun/gussi@cc7a6e3716a7763f599fa73f3e61ec7f3d393271 🚀

---
+++ js/get-cost.js
@@ -0,0 +1,28 @@
+const buttonRemove = document.querySelector(".quantity__button--remove");
+const buttonAdd = document.querySelector(".quantity__button--add");
+const quantityInput = document.querySelector(".quantity__text");
+const costDozens = document.querySelector(".cost__text-dozens");
+const costText = document.querySelector(".cost__text");
+const totalAmount = document.querySelector(".cost__total-amount");
+
+const onButtonRemoveClick = () => {
+  if (quantityInput.value >= 2) {
+    quantityInput.value--;
+    costDozens.textContent = quantityInput.value*120;
+    totalAmount.value = costText.textContent;
+  } else {
+    quantityInput.disabled = true;
+  }
+};
+
+const onButtonAddClick = () => {
+  quantityInput.value++;
+  costDozens.textContent = quantityInput.value*120;
+  totalAmount.value = costText.textContent;
+};
+
+buttonRemove.addEventListener("click", onButtonRemoveClick);
+buttonAdd.addEventListener("click", onButtonAddClick);
+window.addEventListener('load', () => {
+    costDozens.textContent = quantityInput.value*120;
+});
